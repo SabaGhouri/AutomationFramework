90601a2d22c4661f96255e56b34488556dd2f244
raza.abbas50 <sranasd92@gmailcom>
2022-02-23 11:29:18 +0500

    set and get Name

--- src/main/java/Factory/DriverFactory.java
+++ src/main/java/Factory/DriverFactory.java
@@ -5,4 +5,11 @@ public class DriverFactory {
     private String _driver;
     private String Name;
 
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
 }
